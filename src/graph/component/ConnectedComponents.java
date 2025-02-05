6e9232aae0178484fb93ef6948ccd363f0a9f107
arpan jana <arpangroup1@gmail.com>
2025-02-06 03:48:22 +0530

    Graph codes

--- src/graph/component/ConnectedComponents.java
+++ src/graph/component/ConnectedComponents.java
@@ -6,4 +6,8 @@ public class ConnectedComponents {
     public int countConnectedComponents(int n, List<List<Integer>> adj) {
         return 0;
     }
+
+    public int largestComponentCount(int n, List<List<Integer>> adj) {
+        return 0;
+    }
 }
