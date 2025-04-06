2cfc7c352fa96e1fe4b4661212ad962ebe9e16f2
arpan jana <arpangroup1@gmail.com>
2025-04-07 03:24:10 +0530

    NQueen

--- src/recursion/NQueen.java
+++ src/recursion/NQueen.java
@@ -10,7 +10,67 @@ import java.util.*;
 */
 public class NQueen {
     public List<List<String>> solveNQueens(int n) {
-        return null;
+        List<List<String>> result = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) Arrays.fill(board[i], '.');
+        dfs(0, board, result);
+        return result;
+    }
+
+    private void dfs(int col, char[][] board, List<List<String>> result) {
+        if (col == board.length) {
+            result.add(construct(board));
+            return;
+        }
+
+        // try every row for each col
+        for (int row = 0; row < board.length; row++) { // O(N)
+            if (isSafeV1(board, row, col)) { // O(3N)
+                board[row][col] = 'Q';
+                dfs(col + 1, board, result);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isSafeV1(char[][] board, int row, int col) { // O(3N)
+        final int tempRow = row;
+        final int tempCol = col;
+
+        // Check Upper-Left Diagonal
+        while (row >= 0 && col >= 0) {
+            if (board[row][col] == 'Q') return false;
+            row--;
+            col--;
+        }
+
+        // Check Lower-Down Diagonal
+        row = tempRow;
+        col = tempCol;
+        while (row < board.length && col >= 0) {
+            if (board[row][col] == 'Q') return false;
+            row++;
+            col--;
+        }
+
+        // Check Left sides
+        row = tempRow;
+        col = tempCol;
+        while (col >= 0) {
+            if (board[row][col] == 'Q') return false;
+            col--;
+        }
+
+        return true;
+    }
+
+    private List<String> construct(char[][] board) {
+        List < String > res = new LinkedList < String > ();
+        for (int i = 0; i < board.length; i++) {
+            String s = new String(board[i]);
+            res.add(s);
+        }
+        return res;
     }
 
     public static void main(String[] args) {
